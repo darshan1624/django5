19b42e16c012e483068be7f714858587f237e55d
darshan <adarkardarshan228@gmail.com>
2025-07-02 11:35:11 +0530

    Code: Migrations, Model. Theory: Forms

--- ch1/student/models.py
+++ ch1/student/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    city = models.CharField(max_length=200)
+    roll = models.IntegerField()
+    state = models.CharField(max_length=200)
+    comment = models.CharField(max_length=200, default='No comments')
